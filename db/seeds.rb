b11666275de19ef2ef724f94df579fb3425673ae
Nicolas Santibañez <nfsantibanez@uc.cl>
2017-03-31 11:43:29 -0300

    poblar API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Crear datos iniciales para poblar un poco nuestra DB de la application
+
+usuario1 = Usuario.create(usuario: "nfsantibanez", nombre:"Nicolas", apellido:"Santibanez", twitter: "@nfsantiba")
+usuario2 = Usuario.create(usuario: "cacabrera", nombre:"Constanza", apellido:"Cabrera", twitter: "@ccabrera")
+usuario3 = Usuario.create(usuario: "cmcastro", nombre:"Carmen", apellido:"Castro", twitter: "@pelucastro")
+usuario4 = Usuario.create(usuario: "nacuna", nombre:"Norma", apellido:"Acuna", twitter: "@nacuna")
